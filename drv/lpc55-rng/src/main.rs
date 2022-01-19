2bf71220a00d7567a54cf5c081c82839a5957528
Philip Tricca <flihp@twobit.org>
2022-01-19 15:10:31 -0800

    lpc55-rng: Update interface to fill an arbitrary buffer with entropy.
    The RNG hardware on the lpc55 provides a 32bit register holding the
    entropy generated by the platform hardware. The previous interface would
    read a u32 from this register and return it the caller. The rust rand
    crate recommends using a 32 byte seed for CPRNGs[1] and making 8
    syscalls to get this much entropy seems inefficient.
    
    This commit changes the lpc55-rng syscall to fill an arbitrary buffer
    provided by the caller. This new interface is defined using the IDL.
    
    To simplify access to the RNG we add the rng-api library crate that:
    1) wraps the new syscall in the RngCore trait from the 'rand_core' crate
    2) includes a custom 'getrandom' implementation gated by the
    'custom-getrandom' feature
    
    This API crate allows consumers to use the familiar 'getrand' and
    'rand::os::OsRng' interfaces to get random bytes from the rng_server
    task.
    
    [1] https://docs.rs/rand/latest/rand/trait.SeedableRng.html

--- drv/lpc55-rng/src/main.rs
+++ drv/lpc55-rng/src/main.rs
@@ -2,48 +2,96 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-//! Random Number Generation
+//! Driver for the LPC55 random number generator.
 //!
-//! This task will produce random u32 values for you, if you ask nicely.
-//!
-//! An example:
-//!
-//! ```ignore
-//! #[derive(AsBytes)]
-//! #[repr(C)]
-//! struct FetchRandomNumber;
-//!
-//! impl hl::Call for FetchRandomNumber {
-//!     const OP: u16 = 0;
-//!     type Response = u32;
-//!     type Err = u32;
-//! }
-//!
-//! let num = hl::send(rng, &FetchRandomNumber).expect("could not ask the rng for a number");
-//!
-//! hprintln!("got {} from the rng", num).ok();
-//! ```
+//! Use the rng-api crate to interact with this driver.
 
 #![no_std]
 #![no_main]
 
+use core::mem::size_of;
 use drv_lpc55_syscon_api::{Peripheral, Syscon};
+use drv_rng_api::RngError;
+use idol_runtime::{ClientError, RequestError};
+use rand_core::{impls, Error, RngCore};
 use userlib::*;
-use zerocopy::AsBytes;
 
 use lpc55_pac as device;
 
 task_slot!(SYSCON, syscon_driver);
 
-#[repr(u32)]
-enum ResponseCode {
-    BadArg = 1,
-    PoweredOff = 2,
+struct Lpc55Rng {
+    rng: &'static lpc55_pac::rng::RegisterBlock,
 }
 
-impl From<ResponseCode> for u32 {
-    fn from(rc: ResponseCode) -> Self {
-        rc as u32
+impl Lpc55Rng {
+    fn new() -> Self {
+        Lpc55Rng {
+            rng: unsafe { &*device::RNG::ptr() },
+        }
+    }
+}
+
+impl RngCore for Lpc55Rng {
+    fn next_u32(&mut self) -> u32 {
+        impls::next_u32_via_fill(self)
+    }
+    fn next_u64(&mut self) -> u64 {
+        impls::next_u64_via_fill(self)
+    }
+    fn fill_bytes(&mut self, dest: &mut [u8]) {
+        self.try_fill_bytes(dest)
+            .expect("Failed to get entropy from RNG.")
+    }
+    fn try_fill_bytes(&mut self, dest: &mut [u8]) -> Result<(), Error> {
+        let mut cnt = 0;
+        let len = dest.len();
+        const STEP: usize = size_of::<u32>();
+        // fill in multiples of STEP / RNG register size
+        for _ in 0..(len / STEP) {
+            let ent = self.rng.random_number.read().bits();
+            dest[cnt..cnt + STEP].clone_from_slice(&ent.to_ne_bytes());
+            cnt += STEP;
+        }
+        // fill in remaining
+        let remain = len - cnt;
+        assert!(remain < STEP);
+        if remain > 0 {
+            let ent = self.rng.random_number.read().bits();
+            dest[len - remain..].clone_from_slice(&ent.to_ne_bytes());
+        }
+        Ok(())
+    }
+}
+
+impl idl::InOrderRngImpl for Lpc55Rng {
+    fn fill(
+        &mut self,
+        _: &userlib::RecvMessage,
+        dest: idol_runtime::Leased<idol_runtime::W, [u8]>,
+    ) -> Result<usize, RequestError<RngError>> {
+        let mut cnt = 0;
+        const STEP: usize = size_of::<u32>();
+        let mut buf = [0u8; STEP];
+        // fill in multiples of STEP / RNG register size
+        for _ in 0..(dest.len() / STEP) {
+            self.try_fill_bytes(&mut buf)
+                .map_err(|e| RngError::from(e))?;
+            dest.write_range(cnt..cnt + STEP, &buf)
+                .map_err(|_| RequestError::Fail(ClientError::WentAway))?;
+            cnt += STEP;
+        }
+        // fill in remaining
+        let remain = dest.len() - cnt;
+        assert!(remain < STEP);
+        if remain > 0 {
+            self.try_fill_bytes(&mut buf)
+                .map_err(|e| RngError::from(e))?;
+            dest.write_range(dest.len() - remain..dest.len(), &buf)
+                .map_err(|_| RequestError::Fail(ClientError::WentAway))?;
+            cnt += remain;
+        }
+        Ok(cnt)
     }
 }
 
@@ -54,29 +102,24 @@ fn main() -> ! {
 
     syscon.enable_clock(Peripheral::Rng);
 
-    let rng = unsafe { &*device::RNG::ptr() };
     let pmc = unsafe { &*device::PMC::ptr() };
 
-    let mut buffer = [0u32; 1];
+    pmc.pdruncfg0.modify(|_, w| w.pden_rng().poweredon());
+
+    let rng = Lpc55Rng::new();
+    let reseed_threshold = 0x100000; // 1 MiB
+    let mut rng = Lpc55RngServer::new(rng, reseed_threshold)
+        .expect("Failed to create RngServer");
+
+    let mut buffer = [0u8; idl::INCOMING_SIZE];
 
     loop {
-        hl::recv_without_notification(
-            buffer.as_bytes_mut(),
-            |_op: u16, msg| -> Result<(), ResponseCode> {
-                let (_msg, caller) =
-                    msg.fixed::<(), u32>().ok_or(ResponseCode::BadArg)?;
-
-                // if the oscilator is powered off, we won't get good RNG.
-                if pmc.pdruncfg0.read().pden_rng().is_poweredoff() {
-                    return Err(ResponseCode::PoweredOff);
-                }
-
-                let number = rng.random_number.read().bits();
-
-                caller.reply(number);
-
-                Ok(())
-            },
-        );
+        idol_runtime::dispatch(&mut buffer, &mut rng);
     }
+}
+
+mod idl {
+    use drv_rng_api::RngError;
+
+    include!(concat!(env!("OUT_DIR"), "/server_stub.rs"));
 }
