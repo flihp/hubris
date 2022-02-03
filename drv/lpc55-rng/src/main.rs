6d4fe44502557789799a9e30cda58496ea4a7370
Philip Tricca <flihp@twobit.org>
2022-02-02 17:17:02 -0800

    drv-lpc55-rng: Initialize and configure RNG per user manual.

--- drv/lpc55-rng/src/main.rs
+++ drv/lpc55-rng/src/main.rs
@@ -36,15 +36,105 @@ use lpc55_pac as device;
 
 task_slot!(SYSCON, syscon_driver);
 
+struct Rng {
+    rng: &'static lpc55_pac::rng::RegisterBlock
+}
+
+impl From<&'static lpc55_pac::rng::RegisterBlock> for Rng {
+    fn from(rng: &'static lpc55_pac::rng::RegisterBlock) -> Self {
+        Rng { rng }
+    }
+}
+
+impl Rng {
+    // Initialization per user manual v2.4, section 48.15.5, 2021-10-08
+    fn init(
+        &self,
+        pmc: &lpc55_pac::pmc::RegisterBlock,
+        syscon: Syscon,
+    ){
+        // Enable RNG input clock by clearing power down bit (PDRUNCFG0.PDEN_RNG) and
+        // setting AHB RNG clock bit in AHBCLKCTRL.RNG register (AHBCLKCTRLSET2 =
+        // 0x00002000).
+        pmc.pdruncfg0.modify(|_, w| w.pden_rng().poweredon());
+        syscon.enable_clock(Peripheral::Rng);
+
+        // Assert TRNG RESET by setting PRESETCTRL2.RNG_RST bit.
+        // Release TRNG Reset by clearing PRESETCTRL2.RNG_RST bit. Set other TRNG
+        // registers to the default value.
+        // Note: When the device wakes up from Power Down mode, the TRNG module
+        // reset must be asserted before its use.
+        // reset RNG
+        syscon.enter_reset(Peripheral::Rng);
+        syscon.leave_reset(Peripheral::Rng);
+
+        loop {
+            // For revision 1B, the recommendation is to perform CHI computing only
+            // on one specific unprecise clock by selecting COUNTER_CFG.CLOCK_SEL = 4.
+            // This setting is needed to accumulating linear entropy.
+            // Set COUNTER_CFG.CLOCK_SEL = 4 to perform CHI SQUARED Test and
+            // activate CHI computing with ONLINE_TEST_CFG.ACTIVATE = 1.
+            self.rng.counter_cfg.modify(|_, w| unsafe {
+                w.clock_sel().bits(4)
+            });
+            self.rng.online_test_cfg.modify(|_, w| w.activate().set_bit());
+
+            // At power on ONLINE_TEST_VAL.MIN_CHI_SQUARED value is higher than
+            // ONLINE_TEST_VAL.MAX_CHI_SQUARED. Wait until
+            // ONLINE_TEST_VAL.MIN_CHI_SQUARED decreases and becomes smaller than
+            // ONLINE_TEST_VAL.MAX_CHI_SQUARED value.
+            while
+                self.rng.online_test_val.read().min_chi_squared().bits() >=
+                self.rng.online_test_val.read().max_chi_squared().bits() {}
+
+            // If ONLINE_TEST_VAL.MAX_CHI_SQUARED > 4, program
+            // ONLINE_TEST_CFG.ACTIVATE = 0 (to reset), if COUNTER_CFG.SHIFT4X < 7,
+            // increment COUNTER_CFG.SHIFT4X then go back to step 2. This will start
+            // accumulating entropy.
+            // When ONLINE_TEST_VAL.MAX_CHI_SQUARED < 4, initialization is now
+            // complete.
+            if self.rng.online_test_val.read().max_chi_squared().bits() > 4 {
+                self.rng.online_test_cfg.modify(|_, w| {
+                    w.activate().clear_bit()
+                });
+                if self.rng.counter_cfg.read().shift4x().bits() < 7 {
+                    self.rng.counter_cfg.modify(|r, w| {
+                        unsafe { w.shift4x().bits(r.shift4x().bits() + 1) }
+                    });
+                }
+            } else {
+                break;
+            }
+        }
+    }
+
+    // Read RNG register per user manual v2.4, section 48.15.6, 2021-10-08
+    fn read(&self) -> u32 {
+        // 1. Keep Clocks CHI computing active.
+        // 2. Wait for COUNTER_VAL.REFRESH_CNT to become 31 to refill fresh entropy
+        //    since last reading of a random number.
+        while self.rng.counter_val.read().refresh_cnt().bits() != 31 {}
+        // 3. Read new Random number by reading RANDOM_NUMBER register. This will
+        //    reset COUNTER_VAL.REFRESH_CNT to zero.
+        let number = self.rng.random_number.read().bits();
+        // 4. Perform online CHI computing check by checking
+        //    ONLINE_TEST_VAL.MAX_CHI_SQUARED value. Wait till
+        //    ONLINE_TEST_VAL.MAX_CHI_SQUARED becomes smaller or equal than 4.
+        while self.rng.online_test_val.read().max_chi_squared().bits() > 4 {}
+        // 5. Go to step 2 and read new random number.
+        // NOTE: calling this function again is equivalent to 'go to step 2'
+        number
+    }
+}
+
 #[export_name = "main"]
 fn main() -> ! {
     let syscon = SYSCON.get_task_id();
     let syscon = Syscon::from(syscon);
-
-    syscon.enable_clock(Peripheral::Rng);
-
-    let rng = unsafe { &*device::RNG::ptr() };
     let pmc = unsafe { &*device::PMC::ptr() };
+
+    let rng = Rng::from(unsafe { &*device::RNG::ptr() });
+    rng.init(pmc, syscon);
 
     let mut buffer = [0u32; 1];
 
@@ -71,14 +161,14 @@ fn main() -> ! {
                 let mut cnt = 0;
                 const STEP: usize = 4; // sizeof(u32)
                 for i in 0..(borrow_info.len / STEP) {
-                    let number = rng.random_number.read().bits();
+                    let number = rng.read();
                     borrow.write_at(i * STEP, number);
                     cnt += STEP;
                 }
 
                 let remain = borrow_info.len % STEP;
                 if remain > 0 {
-                    let ent = rng.random_number.read().bits().to_ne_bytes();
+                    let ent = rng.read().to_ne_bytes();
                     borrow.write_fully_at(borrow_info.len - remain, &ent[0..remain]);
                     cnt += remain;
                 }
