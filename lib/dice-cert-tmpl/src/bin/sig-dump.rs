bcae034a17cf5e4b7b68080c5fbe04d56f2277b2
Philip Tricca <flihp@twobit.org>
2022-07-04 20:54:32 -0700

    wip: add tool to lib-csr that does a hexdump of interesting fields
    tbsCertificate is the field of sequence of certificate fields that is
    signed by the CA
    the pub & sig-dump do exactly what they say
    These fields are used in the test harness and these tools are used to
    update the tests when the certs are changed.

---
+++ lib/dice-cert-tmpl/src/bin/sig-dump.rs
@@ -0,0 +1,20 @@
+use dice_cert_tmpl::cert::Cert;
+use std::error;
+
+/// Tool to dump the signature bytes from cert.
+fn main() -> Result<(), Box<dyn error::Error>> {
+    let der = include_bytes!("../../data/deviceid-selfcert-tmpl.der");
+    let mut der = *der;
+
+    let cert = Cert::from_slice(&mut der);
+    let (start, end) = cert.get_sig_offsets().expect("fml");
+
+    for (index, byte) in cert.as_bytes()[start..end].iter().enumerate() {
+        if index % 12 == 11 {
+            println!("{:#04X},", byte);
+        } else {
+            print!("{:#04X}, ", byte);
+        }
+    }
+    Ok(())
+}
