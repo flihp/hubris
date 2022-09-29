160cb9f82ee6ecc4783b7727d35b19c851a4aee9
Philip Tricca <flihp@twobit.org>
2022-09-29 15:53:33 -0700

    dice: Split 'dice' feature into self / mfg, add usart setup & teardown
    Serial mfg is a stub that produces a self signed cert until the mfg commands
    land.

--- lib/dice/src/mfg.rs
+++ lib/dice/src/mfg.rs
@@ -7,6 +7,7 @@ use crate::{
     CertSerialNumber,
 };
 use dice_mfg_msgs::{SerialNumber, SizedBlob};
+use lib_lpc55_usart::Usart;
 use salty::signature::Keypair;
 
 // data returned to caller by MFG
@@ -50,6 +51,44 @@ impl DiceMfg for DeviceIdSelfMfg<'_> {
             cert_serial_number: cert_sn,
             serial_number: dname_sn,
             // TODO: static assert deviceid_cert size < SizedBuf max
+            deviceid_cert: SizedBlob::try_from(deviceid_cert.as_bytes())
+                .expect("deviceid cert to SizedBlob"),
+            intermediate_cert: SizedBlob::default(),
+        }
+    }
+}
+
+pub struct DeviceIdSerialMfg<'a> {
+    keypair: &'a Keypair,
+    _usart: Usart<'a>,
+}
+
+impl<'a> DeviceIdSerialMfg<'a> {
+    pub fn new(keypair: &'a Keypair, usart: Usart<'a>) -> Self {
+        Self {
+            keypair,
+            _usart: usart,
+        }
+    }
+}
+
+impl DiceMfg for DeviceIdSerialMfg<'_> {
+    fn run(self) -> DiceMfgState {
+        // produce self-signed cert till mfg commands land
+        let mut cert_sn: CertSerialNumber = Default::default();
+        let dname_sn =
+            SerialNumber::try_from("0123456789ab").expect("DeviceIdSelf SN");
+
+        let deviceid_cert = DeviceIdSelfCertBuilder::new(
+            &cert_sn.next(),
+            &dname_sn,
+            &self.keypair.public,
+        )
+        .sign(self.keypair);
+
+        DiceMfgState {
+            cert_serial_number: cert_sn,
+            serial_number: dname_sn,
             deviceid_cert: SizedBlob::try_from(deviceid_cert.as_bytes())
                 .expect("deviceid cert to SizedBlob"),
             intermediate_cert: SizedBlob::default(),
