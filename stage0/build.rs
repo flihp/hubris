7aca488d15df5a746178693d9bb5bf818ef5063a
Philip Tricca <flihp@twobit.org>
2022-11-17 13:50:21 -0800

    partial implementation of stage0 dice flash range
    implementation is currently limited to parsing toml from string in
    stage0/build.rs and generating a Range<usize> for the flash.

--- stage0/build.rs
+++ stage0/build.rs
@@ -22,5 +22,47 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         image_id
     )?;
 
+    #[cfg(feature = "dice-mfg")]
+    gen_memory_range(&out)?;
+
+    Ok(())
+}
+
+#[cfg(feature = "dice-mfg")]
+fn gen_memory_range (
+    out_dir: &std::path::PathBuf
+) -> Result<(), Box<dyn std::error::Error>> {
+    use serde::Deserialize;
+    use std::collections::HashMap;
+
+    #[derive(Deserialize, Debug)]
+    struct Region {
+        pub name: String,
+        pub address: u32,
+        pub size: u32,
+    }
+
+    // get this from the env set by xtask
+    let memory = "[[flash]]\nname = \"dice\"\naddress = 0x90000\nsize = 0x800\nread = true\nwrite = true\nexecute = false\n";
+    let memory: HashMap<String, Vec<Region>> = toml::from_str(memory)?;
+    let flash = match memory.get("flash") {
+        Some(out) => out,
+        None => {
+            return Err("key not in memory array: \"flash\", can't build stage0")?;
+        }
+    };
+    // find flash array of tables for region with name = "dice"
+    let dice_mem = flash
+        .iter()
+        .filter(|r| &r.name == "dice")
+        .next()
+        .expect("no memory region with name \"dice\"");
+
+    let mut file = File::create(out_dir.join("memory.rs")).unwrap();
+    writeln!(file,
+             "use core::ops::Range;\n\n\
+             pub const DICE_FLASH: Range<usize> = {}..{};",
+             dice_mem.address, dice_mem.address + dice_mem.size)?;
+
     Ok(())
 }
