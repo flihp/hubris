98edac2776daf9bd0fb8668c869915dd5ce545b7
Philip Tricca <flihp@twobit.org>
2022-12-12 09:30:25 -0800

    rotr: Skeleton impl for root of trust for reporting.

---
+++ task/rotr/src/main.rs
@@ -0,0 +1,101 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+//! Root of trust for reporting task.
+//!
+//! Use the rotr-api crate to interact with this task.
+
+#![no_std]
+#![no_main]
+
+use dice::{AliasData, CertData, HandoffData};
+use idol_runtime::RequestError;
+use ringbuf::{ringbuf, ringbuf_entry};
+use task_rotr_api::RotrError;
+
+#[derive(Copy, Clone, PartialEq)]
+enum Trace {
+    AliasMagic([u8; 16]),
+    CertMagic([u8; 16]),
+    Fwid([u8; MEASUREMENT_LEN]),
+    Record([u8; MEASUREMENT_LEN]),
+    Startup,
+    None,
+}
+
+ringbuf!(Trace, 16, Trace::None);
+
+const MAX_MEASUREMENTS: usize = 16;
+const MEASUREMENT_LEN: usize = 32;
+
+struct RotrServer<'a> {
+    _alias: &'a AliasData,
+    _certs: &'a CertData,
+    offset: usize,
+    measurements: [[u8; MEASUREMENT_LEN]; MAX_MEASUREMENTS],
+}
+
+impl<'a> RotrServer<'a> {
+    fn new(alias: &'a AliasData, certs: &'a CertData) -> Self {
+        Self {
+            _alias: alias,
+            _certs: certs,
+            offset: 0,
+            measurements: [[0u8; MEASUREMENT_LEN]; MAX_MEASUREMENTS],
+        }
+    }
+}
+
+impl idl::InOrderRotrImpl for RotrServer<'_> {
+    fn record(
+        &mut self,
+        _msg: &userlib::RecvMessage,
+        hash: [u8; MEASUREMENT_LEN],
+    ) -> Result<(), RequestError<RotrError>> {
+        if self.offset < MAX_MEASUREMENTS {
+            ringbuf_entry!(Trace::Record(hash));
+
+            self.measurements[self.offset] = hash;
+            self.offset += 1;
+
+            Ok(())
+        } else {
+            Err(RotrError::LogFull.into())
+        }
+    }
+}
+
+#[export_name = "main"]
+fn main() -> ! {
+    ringbuf_entry!(Trace::Startup);
+
+    let cert_data = match CertData::from_mem() {
+        Some(a) => a,
+        None => panic!("CertData"),
+    };
+
+    ringbuf_entry!(Trace::CertMagic(cert_data.magic));
+
+    let alias_data = match AliasData::from_mem() {
+        Some(a) => a,
+        None => panic!("AliasData"),
+    };
+
+    ringbuf_entry!(Trace::AliasMagic(alias_data.magic));
+
+    let fwid = alias_data.alias_cert.get_fwid();
+    ringbuf_entry!(Trace::Fwid(fwid.try_into().unwrap()));
+
+    let mut buffer = [0; idl::INCOMING_SIZE];
+    let mut rotr = RotrServer::new(&alias_data, &cert_data);
+    loop {
+        idol_runtime::dispatch(&mut buffer, &mut rotr);
+    }
+}
+
+mod idl {
+    use task_rotr_api::RotrError;
+
+    include!(concat!(env!("OUT_DIR"), "/server_stub.rs"));
+}
