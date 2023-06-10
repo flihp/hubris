493506e379ea7a97c4feecd9159494ab631b48cd
Philip Tricca <flihp@twobit.org>
2023-06-10 16:59:14 -0700

    task-null-server: a minimal hubris task implemening an idol / IPC server

---
+++ lib/null-api/src/lib.rs
@@ -0,0 +1,20 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+//! API crate for the 'attest' task.
+
+#![no_std]
+
+#[derive(
+    Copy, Clone, Debug, Deserialize, Eq, PartialEq, Serialize, SerializedSize,
+)]
+pub enum NullError {
+    SomethingBad,
+}
+
+use hubpack::SerializedSize;
+use serde::{Deserialize, Serialize};
+use userlib::sys_send;
+
+include!(concat!(env!("OUT_DIR"), "/client_stub.rs"));
