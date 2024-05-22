a9b57502c8d6ef9860e2752d96aa30c323721082
Philip Tricca <flihp@twobit.org>
2024-05-22 13:05:18 -0700

    lpc55-rng: Include DICE derived seed in initial PRNG seed.
    We now construct the intial seed:
    SEED_0 = sha3_256(DICE_SEED | HRNG(1k))

--- drv/lpc55-rng/build.rs
+++ drv/lpc55-rng/build.rs
@@ -2,15 +2,50 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+use anyhow::{anyhow, Context, Result};
+use idol::{server::ServerStyle, CounterSettings};
+use std::{fs::File, io::Write};
+
+mod config {
+    include!("src/config.rs");
+}
+
+use config::DataRegion;
+
+const CFG_SRC: &str = "rng-config.rs";
+
+fn main() -> Result<()> {
     idol::Generator::new()
-        .with_counters(
-            idol::CounterSettings::default().with_server_counters(false),
-        )
+        .with_counters(CounterSettings::default().with_server_counters(false))
         .build_server_support(
             "../../idl/rng.idol",
             "server_stub.rs",
-            idol::server::ServerStyle::InOrder,
-        )?;
+            ServerStyle::InOrder,
+        )
+        .map_err(|e| anyhow!(e))?;
+
+    let out_dir = build_util::out_dir();
+    let dest_path = out_dir.join(CFG_SRC);
+    let mut out =
+        File::create(dest_path).context(format!("creating {}", CFG_SRC))?;
+
+    let data_regions = build_util::task_extern_regions::<DataRegion>()?;
+    if data_regions.is_empty() {
+        return Err(anyhow!("no data regions found"));
+    }
+
+    let region = data_regions
+        .get("dice_rng")
+        .ok_or_else(|| anyhow!("dice_rng data region not found"))?;
+    writeln!(
+        out,
+        r##"use crate::config::DataRegion;
+pub const RNG_DATA: DataRegion = DataRegion {{
+    address: {:#x},
+    size: {:#x},
+}};"##,
+        region.address, region.size
+    )?;
+
     Ok(())
 }
